f81239f8a15854b3618fc5a1ec5c7ed703c390d1
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-06-27 16:36:20 +0600

    Update and rename Horizontal Stack Image.py to Horizontal and Vertical Stack Image.py

--- Horizontal and Vertical Stack Image.py
+++ Horizontal and Vertical Stack Image.py
@@ -2,10 +2,12 @@ import cv2
 import numpy as np
 
 img = cv2.imread('Computer Vision\img & vid\Lenna.png')
-img = cv2.resize(img, (300, 300))
+img = cv2.resize(img, (200, 200))
 
 hor = np.hstack((img, img))
+ver = np.vstack((img, img))
 
-cv2.imshow('Horizontal Stack Image', hor)
+cv2.imshow('Horizontal Image', hor)
+cv2.imshow('Vertical Image', ver)
 
 cv2.waitKey(0)
